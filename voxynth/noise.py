5eeda629ed1b97557351b8afdceb6a4a6bb14702
ahoopes <hoopes@mit.edu>
2024-03-18 22:41:46 -0400

    improve perlin defaults and add more efficient upsamping methods

--- voxynth/noise.py
+++ voxynth/noise.py
@@ -1,5 +1,7 @@
 import math
+import numpy as np
 import torch
+import voxynth
 
 from typing import List
 from torch import Tensor
@@ -7,7 +9,47 @@ from torch import Tensor
 from .filter import gaussian_blur
 
 
-def perlin(shape, smoothing, magnitude=1.0, device=None):
+def smooth_gaussian(shape, sigma, magnitude=1.0, device=None, method='blur'):
+    """
+    Generates a smooth Gaussian noise image.
+
+    Parameters
+    ----------
+    shape : List[int]
+        The desired shape of the output tensor. Can be 2D or 3D.
+    sigma : float
+        The spatial smoothing sigma in voxel coordinates.
+    magnitude : float
+        The standard deviation of the noise.
+    device : torch.device or None, optional
+        The device on which the output tensor is allocated. If None, defaults to CPU.
+    method : 'blur' or 'upsample'
+        Method for noise generation. Upsampling is much faster and more memory efficient
+        for larger sigma values, but at the cost of quality.
+
+    Returns
+    -------
+    Tensor
+        A smooth Gaussian noise image of shape `shape`.
+    """
+    if method == 'blur':
+        noise = torch.normal(0, 1, size=shape, device=device)
+        noise = gaussian_blur(noise.unsqueeze(0), sigma).squeeze(0)
+    elif method == 'upsample':
+        downshape = tuple([int(s // sigma) for s in shape])
+        noise = torch.normal(0, 1, size=(1, 1, *downshape), device=device)
+        mode = 'trilinear' if len(shape) == 3 else 'bilinear'
+        noise = torch.nn.functional.interpolate(noise, shape, mode=mode).view(shape)
+    else:
+        raise ValueError(f'unknown smooth gaussian method `{method}`')
+
+    # in-place normalize
+    noise -= noise.mean()
+    noise *= magnitude / noise.std()
+    return noise
+
+
+def perlin(shape, smoothing=None, magnitude=1.0, weights=None, device=None, method='blur'):
     """
     Generates a perlin noise image.
 
@@ -16,20 +58,48 @@ def perlin(shape, smoothing, magnitude=1.0, device=None):
     shape : List[int]
         The desired shape of the output tensor. Can be 2D or 3D.
     smoothing : float or List[float]
-        The spatial smoothing sigma in voxel coordinates. If a
-        single value is provided, it will be used for all dimensions.
-    magnitude : float or List[float]
-        The standard deviation of the noise across dimensions. If a single value is
-        provided, it will be used for all dimensions.
+        The spatial smoothing sigma(s) in voxel coordinates.
+    magnitude : float
+        The standard deviation of the noise.
+    weights : float or List[float]
+        The weights of the smoothing components (scales). If None, defaults
+        to monotonically increasing weights.
     device : torch.device or None, optional
         The device on which the output tensor is allocated. If None, defaults to CPU.
-
+    method : 'blur' or 'upsample'
+        Method for noise generation. Upsampling is much faster and more memory efficient
+        for larger sigma values, but at the cost of quality.
+        
     Returns
     -------
     Tensor
         A Perlin noise image of shape `shape`.
     """
-    noise = torch.normal(0, 1, size=shape, device=device).unsqueeze(0)
-    noise = gaussian_blur(noise, smoothing).squeeze(0)
+    if smoothing is None:
+        smoothing = 2 ** np.arange(np.log2(max(shape)))[1:]
+    elif np.isscalar(smoothing):
+        return smooth_gaussian(shape, smoothing, magnitude, device=device)
+
+    if len(smoothing) == 1:
+        weights = [None]
+    elif weights is None:
+        weights = np.arange(len(smoothing)) + 1
+
+    noise = None
+    for s, w in zip(smoothing, weights):
+
+        # generate smooth field
+        sample = smooth_gaussian(shape, s, device=device, method=method)
+        if w is not None:
+            sample *= w
+
+        # merge the noise at this scale with the rest
+        if noise is None:
+            noise = sample
+        else:
+            noise += sample
+
+    # in-place normalize
+    noise -= noise.mean()
     noise *= magnitude / noise.std()
     return noise
